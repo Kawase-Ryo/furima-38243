2377335135931f4fea3a799a82c0971b82849982
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-10 16:27:41 +0900

    ユーザー管理機能-7

--- db/schema.rb
+++ db/schema.rb
@@ -13,27 +13,13 @@
 ActiveRecord::Schema.define(version: 2022_08_08_022632) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "category_id", null: false
-    t.integer "status_id", null: false
-    t.integer "shipping_charge_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "shipping_day_id", null: false
-    t.integer "preice", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -54,7 +40,4 @@ ActiveRecord::Schema.define(version: 2022_08_08_022632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "users"
-  add_foreign_key "orders", "items"
-  add_foreign_key "orders", "users"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,43 @@ require 'rails_helper'
     @user = FactoryBot.build(:user)
   end
 
+  describe 'ユーザー新規登録' do
+    context 'ユーザ登録ができる時' do
+      it '全ての入力事項が、存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'メールアドレスが@及び空欄でなければ登録できること' do
+        @user.email = 'test@.com'
+        expect(@user).to be_valid
+      end
+      it 'パスワードが6文字以上半角英数字であれば登録できる' do
+        @user.password = '1234abc'
+        @user.password_confirmation = '1234abc'
+        expect(@user).to be_valid
+      end
+      it '名字が全角（漢字・ひらがな・カタカナ）であれば登録できる' do
+        @user.family_name = '山田'
+        expect(@user).to be_valid
+      end
+      it '名前が全角（漢字・ひらがな・カタカナ）であれば登録できる' do
+        @user.first_name = '陸太郎'
+        expect(@user).to be_valid
+      end
+      it '名字のフリガナが全角（カタカナ）であれば登録できる' do
+        @user.family_name_kana = 'ヤマダ'
+        expect(@user).to be_valid
+      end
+      it '名前のフリガナが全角（カタカナ）であれば登録できる' do
+        @user.first_name_kana = 'リクタロウ'
+        expect(@user).to be_valid
+      end
+      it '生年月日があれば登録できる' do
+        @user.birth_day = '2000-01-01'
+        expect(@user).to be_valid
+    end
+  end
 
-  context '新規登録できない場合' do
+  context 'ユーザ登録ができない時' do
      it "ニックネームが必須であること" do
       @user.nickname = ''
       @user.valid?
@@ -39,10 +74,15 @@ require 'rails_helper'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
      end
-      it 'パスワードが半角英数字混合での入力が必須であること' do
+      it 'パスワードが半角英数字混合での入力が必須であること（数字）' do
         @user.password = '1234567'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+        it 'パスワードが半角英数字混合での入力が必須であること（英字）' do
+          @user.password = 'abcdefg'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
      end
      it "パスワードとパスワード（確認）は、値の一致が必須であること" do
       @user.password = 'ab12345'
@@ -50,23 +90,23 @@ require 'rails_helper'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
      end
-      it 'お名前が（全角）は、名字と名前がそれぞれ必須であること' do
-        @user.family_name = ''
+      it 'お名前が（全角）は、姓（全角）に半角文字が含まれていると登録できない' do
+        @user.family_name = 'ﾔﾏﾀﾞ'
         @user.valid?
         expect(@user.errors.full_messages).to include('Family name is invalid')
       end
-      it 'お名前が（全角）は、名字と名前がそれぞれ必須であること' do
-        @user.first_name = ''
+      it 'お名前が（全角）は、名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = 'ﾘｸﾀﾛｳ'
         @user.valid?
         expect(@user.errors.full_messages).to include('First name is invalid')
       end
-      it 'お名前カナ（全角）は、全角（カタカナ）での入力が必須であること' do
-        @user.family_name_kana = ''
+      it 'お名前カナ（全角）は、全角（カタカナ）で姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.family_name_kana = '山田a1/'
         @user.valid?
         expect(@user.errors.full_messages).to include('Family name kana is invalid')
       end
-      it 'お名前カナ（全角）は、全角（カタカナ）での入力が必須であること' do
-        @user.first_name_kana = ''
+      it 'お名前カナ（全角）は、全角（カタカナ）で名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.first_name_kana = '隆太郎a1/'
         @user.valid?
         expect(@user.errors.full_messages).to include('First name kana is invalid')
       end
@@ -75,5 +115,6 @@ require 'rails_helper'
           @user.valid?
           expect(@user.errors.full_messages).to include("Birth day can't be blank")
       end
-   end
+    end
+  end
 end
