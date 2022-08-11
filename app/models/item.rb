9f56a8a83e7304223bea9f20bd65fc664ceca31b
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-11 18:50:27 +0900

    商品出品機能-1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,35 @@
 class Item < ApplicationRecord
 
      # テーブルとのアソシエーション
-     #belongs_to :user
-     #has_one    :order
+     belongs_to :user
+     has_one    :order
 
-     #with_options presence: true do
-        #validates :name
-        #validates :description
-        #validates :category_id
-        #validates :status_id
-        #validates :shipping_charge_id
-        #validates :prefecture_id
-        #validates :shipping_day_id
-     #end
+     # アクティブハッシュとのアソシエーション
+     belongs_to :category
+     belongs_to :status
+     belongs_to :shipping_charge
+     belongs_to :prefecture
+     belongs_to :shipping_day
+
+     # active_storageとのアソシエーション（items・active_storage_blobsテーブルを関連付け）
+     has_one_attached :image
+
+     with_options presence: true do
+        validates :name
+        validates :description
+        validates :category_id
+        validates :status_id
+        validates :shipping_charge_id
+        validates :prefecture_id
+        validates :shipping_day_id
+     end
+
+     # ジャンルの選択が「--」の時は保存不可
+     with_options numericality: { other_than: 0 } do
+        validates :category_id
+        validates :status_id
+        validates :shipping_charge_id
+        validates :prefecture_id
+        validates :shipping_day_id
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,18 @@
 ActiveRecord::Schema.define(version: 2022_08_08_022632) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_charge_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "preice", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -40,4 +50,5 @@ ActiveRecord::Schema.define(version: 2022_08_08_022632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
