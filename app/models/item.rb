6183ccfdb0aa8e6edfb7218f449e998425f0b7dc
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-12 21:49:01 +0900

    商品出品機能-3

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
 
      # テーブルとのアソシエーション
      belongs_to :user
-     has_one    :order
+     #has_one    :order
 
      # アクティブハッシュとのアソシエーション
      belongs_to_active_hash :category
@@ -16,7 +16,6 @@ class Item < ApplicationRecord
      has_one_attached :image
 
      with_options presence: true do
-        validates :user_id
         validates :image
         validates :name
         validates :description
@@ -25,7 +24,7 @@ class Item < ApplicationRecord
         validates :shipping_charge_id
         validates :prefecture_id
         validates :shipping_day_id
-        validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+        validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
      end
 
      # ジャンルの選択が「--」の時は保存不可
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe Item, type: :model do
       it 'ユーザー登録している人でないと出品できない' do
         @item.user_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist', "User can't be blank")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
       it '１枚画像がないと出品できない' do
         @item.image = nil
@@ -121,6 +121,11 @@ RSpec.describe Item, type: :model do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+        it '価格に半角数字以外が含まれている場合は出品できない（※半角数字以外が一文字でも含まれていれば良い）' do
+          @item.price ="３００"
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
