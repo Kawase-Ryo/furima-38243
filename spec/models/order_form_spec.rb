a7cc4ccfb01c6240327d429922443944d67c47f6
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-17 21:11:56 +0900

    日本語表記-7

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -60,57 +60,57 @@ describe '配送先情報の保存' do
     it '郵便番号が空だと保存できないこと' do
       @order_form.post_code = nil
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Post codeを入力してください")
+      expect(@order_form.errors.full_messages).to include("郵便番号を入力してください")
     end
     it '郵便番号にハイフンがないと保存できないこと' do
       @order_form.post_code = 1_234_567
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include('Post codeis invalid. Include hyphen(-)')
+      expect(@order_form.errors.full_messages).to include('郵便番号is invalid. Include hyphen(-)')
     end
     it '都道府県が「---」だと保存できないこと' do
       @order_form.prefecture_id = 0
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Prefecturecan't be blank")
+      expect(@order_form.errors.full_messages).to include("都道府県can't be blank")
     end
     it '都道府県が空だと保存できないこと' do
       @order_form.prefecture_id = nil
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Prefectureを入力してください")
+      expect(@order_form.errors.full_messages).to include("都道府県を入力してください")
     end
     it '市区町村が空だと保存できないこと' do
       @order_form.city = nil
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Cityを入力してください")
+      expect(@order_form.errors.full_messages).to include("市区町村を入力してください")
     end
     it '番地が空だと保存できないこと' do
       @order_form.address = nil
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Addressを入力してください")
+      expect(@order_form.errors.full_messages).to include("番地を入力してください")
     end
     it '電話番号が空だと保存できないこと' do
       @order_form.phone_number = nil
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Phone numberを入力してください")
+      expect(@order_form.errors.full_messages).to include("電話番号を入力してください")
     end
     it '電話番号にハイフンがあると保存できないこと' do
       @order_form.phone_number = '123 - 1234 - 1234'
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include('Phone numberis invalid')
+      expect(@order_form.errors.full_messages).to include('電話番号is invalid')
     end
     it '電話番号が9桁以下あると保存できないこと' do
       @order_form.phone_number = 12_345_678
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include('Phone numberis invalid')
+      expect(@order_form.errors.full_messages).to include('電話番号is invalid')
     end
     it '電話番号が12桁以上あると保存できないこと' do
       @order_form.phone_number = 12_345_678_910_123_111
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include('Phone numberis invalid')
+      expect(@order_form.errors.full_messages).to include('電話番号is invalid')
     end
     it 'トークンが空だと保存できないこと' do
       @order_form.token = nil
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Tokenを入力してください")
+      expect(@order_form.errors.full_messages).to include("クレジットカードを入力してください")
     end
   end
 end
